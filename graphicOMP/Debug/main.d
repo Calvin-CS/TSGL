ccd1567ebe2c558caf224a531bc9bc48d01cea87
pac3 <pac3@students.calvin.edu>
2014-06-03 11:33:03 -0400

    Updated with main.cpp

---
+++ graphicOMP/Debug/main.d
@@ -0,0 +1,82 @@
+main.d: ../main.cpp \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/fl_utf8.h \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Export.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/fl_types.h \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Xutf8.h \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Enumerations.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Window.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Group.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Widget.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Text_Buffer.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Text_Display.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/fl_draw.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/x.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Window.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Enumerations.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Device.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Plugin.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Preferences.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Image.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Bitmap.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Image.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Pixmap.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_RGB_Image.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Scrollbar.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Slider.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Valuator.H \
+ /home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Text_Buffer.H
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/fl_utf8.h:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Export.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/fl_types.h:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Xutf8.h:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Enumerations.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Window.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Group.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Widget.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Text_Buffer.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Text_Display.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/fl_draw.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/x.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Window.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Enumerations.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Device.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Plugin.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Preferences.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Image.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Bitmap.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Image.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Pixmap.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_RGB_Image.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Scrollbar.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Slider.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Valuator.H:
+
+/home/mbv26/summer2014/graphicOMP.git/libraries/include/FL/Fl_Text_Buffer.H:
---
+++ graphicOMP/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lfltk -lfltk_forms -lfltk_images -lfltk_jpeg -lfltk_png -lX11
+
