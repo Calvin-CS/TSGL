eb0fe3739a7a80c83f8208005f136958aad64d38
pac3 <pac3@students.calvin.edu>
2014-06-05 10:06:07 -0400

    Documented everything so far

--- graphicOMP/Point.h
+++ graphicOMP/Point.h
@@ -1,41 +1,55 @@
+// Points extends Shape and provides a class for drawing a single point to a Canvas
 //
-// Basic Point class build on top of FLTK library.
-// UNFINISHED
-//
-// Last Modified: Patrick Crain, 6/4/2014
-//
-
+// Last Modified: Patrick Crain, 6/5/2014
 #ifndef POINT_H_
 #define POINT_H_
 
-#include <FL/fl_draw.H>
-#include "Shape.h"
-#include <iostream>
+#include <FL/fl_draw.H>		//For the actual fl_point drawing function
+#include "Shape.h"			//For extending our Shape object
 
 class Point : public Shape {
 private:
-	int myX, myY;
+	int myX, myY;										//Positioning data for our Point
 public:
-    inline Point(int x, int y);
-    inline Point(int x, int y, int r, int g, int b);
-    inline virtual ~Point();
-    inline void draw();
+    inline Point(int x, int y);							//Default constructor
+    inline Point(int x, int y, int r, int g, int b);	//Explicit constructor
+    inline virtual ~Point();							//Default destructor
+    inline void draw();									//Overridden draw method
 };
 
+/*
+ * Explicit constructor for the Point class (calls the base constructor)
+ * Parameters:
+ * 		x, the x coordinate
+ *		y, the y coordinate
+ * Returns: a new Point at the specified position
+ */
 Point::Point(int x, int y) : Shape() {
 	myX = x;
 	myY = y;
 }
 
+/*
+ * Explicit constructor for the Point class (calls the base constructor)
+ * Parameters:
+ * 		x, the x coordinate
+ *		y, the y coordinate
+ * 		r, the red component
+ * 		g, the green component
+ * 		b, the blue component
+ * Returns: a new Point at the specified position with the specified color
+ */
 Point::Point(int x, int y, int r, int g, int b) : Shape(r, g, b) {
 	myX = x;
 	myY = y;
 }
 
+// Destructor for the Point class
 Point::~Point() {}
 
+// draw actually draws the Point to the canvas
 void Point::draw() {
-	fl_point(myX,myY);
+	fl_point(myX,myY);	//Call to the FLTK fl_point drawing method
 }
 
 #endif /* POINT_H_ */
