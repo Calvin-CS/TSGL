b254fd85742c2ca9abb2900be9df8b43b8b39896
Mark Vander Stel <mbv26@students.calvin.edu>
2014-06-09 16:57:09 -0400

    Got rid of that pesky Debug folder. Hmpf.

--- graphicOMP/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lfltk_images -lfltk -lfltk_jpeg -lfltk_png -lfltk_forms -lX11 -lz
-
