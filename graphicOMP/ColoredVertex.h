19b852299de10abac884d5e6dc4294196f953343
pac3 <pac3@students.calvin.edu>
2014-07-01 10:18:09 +0000

    ColoredVertex Added...again

---
+++ graphicOMP/ColoredVertex.h
@@ -0,0 +1,13 @@
+#ifndef COLOREDVERTEX_H_
+#define COLOREDVERTEX_H_
+
+#include "color.h"
+
+struct ColoredVertex {
+public:
+	float x;
+	float y;
+	RGBfloatType c;
+};
+
+#endif /* COLOREDVERTEX_H_ */
