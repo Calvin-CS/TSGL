da1ae1f8579057f56e5d9c97921b0f4213a64a9e
Mark Vander Stel <mbv26@students.calvin.edu>
2014-07-08 10:14:10 -0400

    Added pastPeriod() to allow for checking if its time without having to sleep

--- graphicOMP/Timer.h
+++ graphicOMP/Timer.h
@@ -2,7 +2,7 @@
  * Timer.h provides a simple timer for sleeping and keeping track of iterations
  *
  * Authors: Patrick Crain, Mark Vander Stel
- * Last Modified: Mark Vander Stel, 6/20/2014
+ * Last Modified: Mark Vander Stel, 7/8/2014
  */
 
 #ifndef TIMER_H_
@@ -17,23 +17,39 @@ class Timer {
 private:
 	highResClock::time_point startTime, lastTime;
 	std::chrono::duration<double> period_;
+	int lastRep;
 public:
 	Timer(double period) {
 		startTime = lastTime = highResClock::now();
 		period_ = std::chrono::duration<double>(period);
+		lastRep = 0;
 	}
 
 	virtual ~Timer() {}
 
+	// Get the number of reps since start
+	int getReps() {
+		return (highResClock::now() - startTime) / period_;
+	}
+
+	// Get the time since start
+	double getTime() {
+		return	std::chrono::duration_cast<std::chrono::nanoseconds>(
+				highResClock::now() - startTime).count() / 1000000000.0;
+	}
+
+	bool pastPeriod() {
+		if (lastRep < getReps()) {
+			lastRep = getReps();
+			return true;
+		} else return false;
+	}
+
+	// Sleep the thread until the period has passed
 	void sleep() {
 		std::this_thread::sleep_until(lastTime + period_);
 		lastTime = highResClock::now();
 	}
-
-	double getTime() { return	 std::chrono::duration_cast<std::chrono::nanoseconds>(
-								 highResClock::now() - startTime).count() / 1000000000.0; }
-
-	int getReps() { return (highResClock::now() - startTime) / period_; }
 };
 
 #endif /* TIMER_H_ */
