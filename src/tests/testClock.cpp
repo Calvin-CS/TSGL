74d994573ca1021d6e383b56fcd95306b392394e
Kirby Bubbles <codemonkey941@gmail.com>
2015-06-03 17:37:38 -0400

    Found one of the problems with CartesianCanvas' drawRectangle() method. Made testClock a temporary testbed for testing CartesianCanvas methods (to see if they draw correctly).

--- src/tests/testClock.cpp
+++ src/tests/testClock.cpp
@@ -9,22 +9,18 @@
 
 const int WINDOW_W = 400*3, WINDOW_H = 300*3, BUFFER = WINDOW_W * WINDOW_H * 2;
 
-void clockFunction(Canvas& can) {
-  const int WINDOW_W = can.getWindowWidth(),
-            WINDOW_H = can.getWindowHeight(),
-            RADIUS = WINDOW_W / 2 - 32;
-  int centerX = WINDOW_W / 2;
-  int centerY = WINDOW_H / 2;
-  while(can.getIsOpen()) {
-  can.sleep();
-  can.drawCircle(centerX, centerY, RADIUS, 64, BLACK, false);
-  can.drawLine(centerX + 20, centerY, centerX + 166, centerY - 32);
-  can.clear();
-  }
+typedef CartesianCanvas Cart;
+
+//Testbed for fixing CartesianCanvas
+//Will be a Clock test once CartesianCanvas methods draw correctly
+//(see "Clean up little TSGL issues" on Trello
+void clockFunction(Cart& can) {
+  can.drawAxes(0, 0, 10, 10);
+  can.drawRectangle(200, 60, 50, 100, BLACK, false);
 }
 
 int main() {
-  Canvas c1(0, 0, 400, 400, "");
+  Cart c1;
   c1.setBackgroundColor(WHITE);
   c1.start();
   clockFunction(c1);
