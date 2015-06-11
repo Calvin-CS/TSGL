0e9668f4e9c498d51f972445d95bd0ce1d34ea5a
Kirby Bubbles <codemonkey941@gmail.com>
2015-06-11 11:07:27 -0400

    Made changes to the Unit tests so that now they have colors indicating a pass or fail. Added some to ConcavePolygon.

--- src/TSGL/TsglAssert.h
+++ src/TSGL/TsglAssert.h
@@ -9,6 +9,7 @@
 #define SRC_TSGL_TSGLASSERT_H_
 
 #include <iostream>
+#include "Error.h"
 #ifdef _WIN32
 #include <string>
 #endif
@@ -16,11 +17,18 @@
 namespace tsgl {
 
 /*!
- *
+ * \brief Assertion used in Unit testing.
+ * \details Assertion mechanism used in Unit testing where a function
+ * that returns a boolean is passed and an error message is passed if
+ * the function returns false.
+ *    \param b A boolean returned from a Unit test function.
+ *    \param message A string representing the error message to print out if b is false.
+ * \note Can also pass a boolean expression; it doesn't necessarily have to be a function.
+ * \return Nothing if the function returns true, message in red text if it returns false.
  */
 static void tsglAssert(bool b, std::string message) {
     if(!b) {
-     std::cerr << message << std::endl;
+     TsglErr(message);  //Changed it so that now it uses the TsglErr() function
     }
 }
 
