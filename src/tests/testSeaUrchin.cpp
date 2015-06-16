1c6cbcd3644fc001a30c94ee859040b77b9ac056
Kirby Bubbles <codemonkey941@gmail.com>
2015-06-16 17:53:04 -0400

    Made more progress in the testSeaUrchin test.

--- src/tests/testSeaUrchin.cpp
+++ src/tests/testSeaUrchin.cpp
@@ -15,24 +15,42 @@ using namespace tsgl;
 void seaUrchinFunction(Canvas& can) {
   const int CENTER_X = can.getWindowWidth() / 2, CENTER_Y = can.getWindowHeight() / 2;
   while(can.getIsOpen()) {
-   can.sleep();
-   float delta = 2.0f / 32 * 3.1415926585f;
-   int x = 250;
-   int y = 250;
-   int oldX = 250;
-   int oldY = 250;
-   int newX, newY = 0;
-   for(int i = 0; i < 32; ++i) {
-     newX = oldX + 50 * cos(i*delta + can.getReps());
-     newY = oldY + 50 * sin(i*delta + can.getReps());
-     can.drawLine(oldX, oldY, newX, newY);
-     can.clear();
-//    can.drawCircle(CENTER_X, CENTER_Y, 200, 32, BLACK, false);
-//    can.drawLine(CENTER_X, CENTER_Y, CENTER_X, CENTER_Y - 150);
-//    can.drawLine(CENTER_X, CENTER_Y, CENTER_X + 100, CENTER_Y);
-   }
+    srand(time(NULL));
+    can.sleep();
+    float delta = 2.0f / 32 * 3.1415926585f;
+    int x = 250;
+    int y = 250;
+    int oldX = 250;
+    int oldY = 250;
+    int newX, newY = 0;
+    for(int i = 0; i < 32; ++i) {
+      //Plus = clockwise
+      //Minus = counterclockwise
+      newX = oldX + 50 * cos(i *delta + can.getReps());
+      newY = oldY + 200* sin(i *delta + can.getReps());
+      can.drawLine(oldX, oldY, newX, newY);
+      can.clear();
+    }
+    newX = newY = 0;
+    oldX = oldY = 150;
+    for(int j = 0; j < 64; ++j) {
+      newX = oldX + 50 * cos(j *delta + can.getReps());
+      newY = oldY + 50 * sin(j *delta + can.getReps());
+      can.drawLine(oldX, oldY, newX, newY);
+      can.clear();
+    }
+
+    oldX = 50;
+    oldY = 400;
+    for(int k = 0; k < 128; ++k) {
+      newX = oldX + 50 * cos(k *delta + can.getReps());
+      newY = oldY + 50 * sin(k *delta + can.getReps());
+      can.drawLine(oldX, oldY, newX, newY);
+      can.clear();
+    }
   }
 }
+
 int main() {
   Canvas c1(0, 0, 500, 500, "", FRAME);
   c1.setBackgroundColor(WHITE);
