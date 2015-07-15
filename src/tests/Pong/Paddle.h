7605b6e9ee7ab77ab38b7af190deaa6da4f4b7bf
Kirby Bubbles <codemonkey941@gmail.com>
2015-07-15 10:12:05 -0400

    Finished documenting the new classes and the Pong class.

--- src/tests/Pong/Paddle.h
+++ src/tests/Pong/Paddle.h
@@ -13,57 +13,72 @@
 
 /*!
  * class Paddle
- * \brief Let's play some Pong!
+ * \brief How can you hit the ball without a paddle?
  * \details Creates a Paddle in order to play the game, Pong.
- * \details
- * \details
- * \details
+ * \details The W and S keys move the left paddle, the Up and Down arrow keys move the right paddle in the game.
+ * \details Collisions with the ball are not handled here; they are handled in the Pong class' draw() method.
+ * \details The left and right paddles in the game are created from this class; they are essentially the same object.
+ * However, there are side parameters in some of the methods that determine which side to place the Paddle object on and how to treat
+ * the Paddle object (either as the left or the right paddle in the game). The Pong class handles how to distribute points to the Paddle objects
+ * in the game (the Paddle class only has an increment() method that increments its score counter; the Pong class determines which object should
+ * call that method when a point is earned).
+ * \see Ball class, Pong class.
  */
 class Paddle {
 public:
+
   /*!
    * \brief Explicitly constructs a Paddle object.
    * \details Explicit constructor for a Paddle object.
-   * \param can Reference to the Canvas
+   * \param can Reference to the Canvas to have the Paddle object on.
    * \return The constructed Paddle object.
    */
   Paddle(Canvas& can);
 
   /*!
    * \brief Binds the buttons.
-   * \details Binds the buttons with the Canvas.
+   * \details Binds the buttons with the Canvas. In this case, the keys that move the paddle up and down.
+   * \param can Reference to the Canvas to bind the keys to.
+   * \param side The side that the Paddle object is on (left = -1 and the W and S keys are bound, right = 1 and the Up and Down arrow keys are bound).
    */
   void bindings(Canvas& can, int side);
 
   /*!
-   * \brief
+   * \brief Draw the Paddle object.
+   * \details Actually draws the Paddle object onto the Canvas.
+   * \param can Reference to the Canvas to draw on.
+   * \param side The side that the Paddle object is drawn to on the Canvas (left = -1, right = 1).
    */
   void draw(Canvas& can, int side);
 
   /*!
-   * \brief
+   * \brief Increments the Paddle object's score in the game of Pong.
    */
   void increment();
 
   /*!
-   * \brief
+   * \brief Actually Moves the Paddle object up or down.
    */
   void move();
 
   /*!
-   * \brief
+   * \brief Accessor for the score of the Paddle object.
+   * \return myPoints The current score of the Paddle object in the game of Pong.
    */
   int getPoints() const;
 
   /*!
-   * \brief
+   * \brief Accessor for the current y-coordinate of the Paddle object.
+   * \return myY The y-coordinate of the Paddle object.
    */
   float getY() const;
 
   /*!
-   * \brief
+   * \brief Mutator for the direction of the Paddle object.
+   * \details Changes the current direction of the Paddle object (up or down).
    */
   void setDir(int direction);
+
 private:
   int myDir;  //-1 = up, 1 = down, 0 = stationary
   int myPoints;  //Score
--- src/tests/Pong/Pong.h
+++ src/tests/Pong/Pong.h
@@ -19,10 +19,36 @@
 
 using namespace tsgl;
 
+/*!
+ * \class Pong
+ * \brief An old-school classic!
+ * \details Draw the interactive game of Pong. The two paddles are objects and the ball is also an object.
+ * \details The constructor constructs the two paddle objects and the ball object.
+ * \details Use the W and S keys in order to move the left paddle, the Up and Down arrow keys in order to move the right paddle.
+ * \details Everything else is handled in the draw() method (button bindings, score keeping, drawing the objects and the game itself,
+ * ball collisions with the paddles and the boundaries).
+ * \see Paddle class, Ball class.
+ */
 class Pong {
 public:
+
+  /*!
+   * \brief Explicitly construct the game, Pong.
+   * \details Explicit constructor for the game, Pong. It sets up the Paddle objects and the Ball object
+   * in order to play.
+   * \param can Reference to the Canvas to use when playing Pong.
+   */
   Pong(Canvas& can);
+
+  /*!
+   * \brief Draw the game of Pong.
+   * \details Actually draws all of the necessary components in order to play Pong.
+   * That also includes any necessary button bindings in order to move the Paddle objects.
+   * \param can Reference to the Canvas to draw on.
+   * \see Paddle class, Ball class.
+   */
   void draw(Canvas& can);
+
 private:
   Paddle * leftPaddle;
   Paddle * rightPaddle;
