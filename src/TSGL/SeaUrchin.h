242d130c3647a90d4044950b623775edaedd79b6
Kirby Bubbles <codemonkey941@gmail.com>
2015-07-16 17:08:05 -0400

    Documented SeaUrchin class. :D.

--- src/TSGL/SeaUrchin.h
+++ src/TSGL/SeaUrchin.h
@@ -14,8 +14,29 @@ using namespace tsgl;
 
 class SeaUrchin {
 public:
+
+  /*!
+   * \brief Explicitly construct a SeaUrchin object.
+   * \details Explicit constructor for a SeaUrchin object.
+   * \param can Reference to the Canvas to draw to.
+   * \param threadId The thread that is currently creating a SeaUrchin object.
+   * \param colorScheme 0 equals all of the SeaUrchin objects created are the same color, 1 equals the inverse.
+   * \return The constructed SeaUrchin object.
+   */
   SeaUrchin(Canvas& can, int threadId, int colorScheme);  //Default constructor
+
+  /*!
+   * \brief Draw the SeaUrchin.
+   * \details Actually draws the SeaUrchin object onto the Canvas.
+   * \param can Reference to the Canvas object to draw to.
+   */
   void draw(Canvas& can);
+
+  /*!
+   * \brief Destroy a SeaUrchin.
+   * \details Destructor for a SeaUrchin object.
+   * \note This function does absolutely nothing.
+   */
   virtual ~SeaUrchin();
 
 private:
