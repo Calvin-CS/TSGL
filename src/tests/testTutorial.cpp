9d891715407bb254f9a22f9e975b0542f50818f8
Kirby Bubbles <codemonkey941@gmail.com>
2015-08-18 14:04:11 -0400

    Test Update Doxygen.

--- src/tests/testTutorial.cpp
+++ src/tests/testTutorial.cpp
@@ -1,24 +1,36 @@
 #include <tsgl.h>
 using namespace tsgl;
 
-int main() {
-  Canvas c(0, 0, 500, 600, "Animation Loop Example", FRAME / 2);
-  c.start();
-  //Store the x and y-coordinate values in variables outside of the loop
-  int x = 250, y = 300;
-  //Drawing loop
-  while(c.isOpen()) {
-    c.sleep();
-    //Check to see if we are off screen
-    if (x >= c.getWindowWidth()) {
-       x = 250; //If so, reset the x-coordinate
-    }
-    //Let's draw a circle!
-    //Pass the x and y coordinates
-    c.drawCircle(x, y, 50, 32);
-    //And change the x-coordinate once the circle has been drawn
-    x += 5;
-    c.clear(); //New statement
+
+void drawTutorial(Canvas & can, int numberOfThreads) {
+  const unsigned int width = can.getWindowWidth(), height = can.getWindowHeight();
+  can.bindToButton(TSGL_SPACE, TSGL_PRESS, [&can] () {
+    can.clear();
+  });
+#pragma omp parallel num_threads(numberOfThreads)
+{
+  float tid = omp_get_thread_num(), nthreads = omp_get_num_threads();
+  ColorFloat color = Colors::highContrastColor(tid);
+  unsigned bstart = tid*(width/nthreads);
+  unsigned bend = (tid==nthreads) ? width-1 : bstart + width/nthreads;
+  for (unsigned i = bstart; i <= bend; i++)
+    for (unsigned int j = 0; j <= height; j++)
+      can.drawPoint(i, j, color);
   }
+}
+
+int main(int argc, char * argv[]) {
+  int width = (argc > 1) ? atoi(argv[1]) : 700;
+  int height = (argc > 2) ? atoi(argv[2]) : 800;
+  if (width <= 0 || height <= 0) {
+    width = height = 900;
+  }
+  int threads = (argc > 3) ? atoi(argv[3]) : omp_get_num_procs();
+  if(threads <= 0) {
+    threads = omp_get_num_procs();
+  }
+  Canvas c(0, 0, width, height, "Threads", FRAME);
+  c.start();
+  drawTutorial(c, threads);
   c.wait();
 }
