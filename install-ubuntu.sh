e4945b44652a02f933490134488a3e1ff6872164
samuelth47 <sth5@students.calvin.edu>
2021-06-24 16:24:59 -0400

    Small change to install-ubuntu.sh.

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -284,12 +284,15 @@ echo "All dependencies resolved!"
 
 ###################################################################################
 
-#change the call for assets and LFLAGS directory
-cd src/examples
-grep -rli '/usr/local/include/TSGL/assets' * | xargs -i@ sed -i's|/usr/local/include/TSGL/assets|/usr/include/TSGL/assets|g' @
+#Add export env variables to .bashrc!!!!!!!!!!!!!!!!!!!
 
-grep -rli 'L/usr/local/lib' * | xargs -i@ sed -i 's|L/usr/local/lib|L/usr/lib|g' @
-cd -
+
+#change the call for assets and LFLAGS directory
+#cd src/examples
+#grep -rli '/usr/local/include/TSGL/assets' * | xargs -i@ sed -i's|/usr/local/include/TSGL/assets|/usr/include/TSGL/assets|g' @
+
+#grep -rli 'L/usr/local/lib' * | xargs -i@ sed -i 's|L/usr/local/lib|L/usr/lib|g' @
+#cd -
 
 ###################################################################################
 
